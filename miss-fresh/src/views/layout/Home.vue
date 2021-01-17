d54ac8266cd250b90c229b282476419df745bb46
web-wj <451959831@qq/com>
2021-01-17 12:03:57 +0800

    小修小补

--- miss-fresh/src/views/layout/Home.vue
+++ miss-fresh/src/views/layout/Home.vue
@@ -54,12 +54,17 @@
         </a-sub-menu>
       </a-menu>
     </div>
-    <div :class="{'main-app':true,'menu-unfold':collapsed}">
+    <div :class="{ 'main-app': true, 'menu-unfold': collapsed }">
       <div class="main-header">
         <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
           <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
         </a-button>
-        <div class="breadcrumb">面包屑</div>
+        <div class="breadcrumb">
+          <a-breadcrumb>
+            <a-breadcrumb-item><a href="">首页</a></a-breadcrumb-item>
+            <a-breadcrumb-item>统计</a-breadcrumb-item>
+          </a-breadcrumb>
+        </div>
         <ul class="user-info">
           <li>欢迎：wj<a-icon type="down" /></li>
           <li>退出</li>
