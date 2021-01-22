3075a6cc713a48510f0697ca994fda1d8896f821
web-wj <451959831@qq/com>
2021-01-22 21:28:13 +0800

    Index-Page-over

--- miss-fresh/src/axios.js
+++ miss-fresh/src/axios.js
@@ -3,13 +3,9 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://mallapi.duyiedu.com/',
 });
-instance.interceptors.request.use((config) => {
-  console.log(config);
-  return config;
-}, (error) => Promise.reject(error));
+instance.interceptors.request.use((config) => config, (error) => Promise.reject(error));
 
 instance.interceptors.response.use((response) => {
-  console.log(response);
   if (response.data.status === 'fail') {
     return Promise.reject(response.data.msg);
   }
--- miss-fresh/src/views/pages/Index.vue
+++ miss-fresh/src/views/pages/Index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="index">
     <card />
+    <line-charts />
   </div>
 </template>
 
 <script>
 import Card from '../../components/Card.vue';
+import LineCharts from '../../components/LineCharts.vue';
 
 export default {
   components: {
     Card,
+    LineCharts,
   },
 };
 </script>
