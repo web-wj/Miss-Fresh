f0852bd91706996c1ab422d7be3a25ee71655228
web-wj <451959831@qq/com>
2021-02-01 10:45:51 +0800

    配置

---
+++ miss-fresh-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<style lang="less">
+*{
+   padding: 0;
+   margin: 0;
+}
+#app {
+  width: 375px;
+  height: 100px;
+  background-color: antiquewhite;
+}
+</style>
---
+++ miss-fresh-app/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vant from 'vant';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'vant/lib/index.css';
+import 'lib-flexible';
+
+Vue.use(Vant);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
